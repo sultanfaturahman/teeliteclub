f56460cd78c06b7d7303a5b01f4acc99271e09db
sultanfaturahman <sultannfaturahman@gmail.com>
2025-11-08 14:44:48 +0700

    final

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -147,7 +147,7 @@ const Index = () => {
                         {countdownMessage}
                       </p>
                     )}
-                    <div className="w-full flex pb-10 pt-1 justify-center">
+                    <div className="w-full flex pb-12 pt-1 justify-center">
                       <MaintenanceCountdown
                         target={maintenanceTarget}
                         tone="dark"
